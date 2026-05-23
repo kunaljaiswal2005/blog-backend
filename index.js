98c16f3886b7e932c72c1d882f5e35cf9ce37486
KUNAL JAISWAL <kunaljaiswal1402@gmail.com>
2026-05-23 16:32:53 +0530

    Fixed CORS

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://blog-frontend-pi-seven.vercel.app",
+    ],
     credentials: true,
   }),
 );
+
 app.use(express.json());
 
 // Routes
@@ -21,7 +25,10 @@ app.use("/api/comments", require("./routes/comments"));
 
 // Health check
 app.get("/api/health", (req, res) => {
-  res.json({ status: "OK", message: "Server is running" });
+  res.json({
+    status: "OK",
+    message: "Server is running",
+  });
 });
 
 // Connect MongoDB
@@ -31,4 +38,5 @@ mongoose
   .catch((err) => console.log("❌ DB Error:", err));
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
